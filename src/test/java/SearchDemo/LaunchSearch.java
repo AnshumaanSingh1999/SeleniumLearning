1d3efa1ef33ce38005f2e448117593c798c24e76
Anshumaan Singh <anshumaanborntolead@gmail.com>
2024-11-21 13:06:06 +0530

    changes

--- src/test/java/SearchDemo/LaunchSearch.java
+++ src/test/java/SearchDemo/LaunchSearch.java
@@ -12,7 +12,7 @@ public class LaunchSearch {
 	public static void main(String[] args) {
 
 		System.out.print("Automation Task Running");
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\anshumaan.singh\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\Downloads\\SeleniumLearning\\src\\res\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 	 
